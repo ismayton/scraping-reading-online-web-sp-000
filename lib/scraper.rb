26ac460cb0b6329d787abf63d903de196ea2b6c0
Ian Mayton <ian.mayton@gmail.com>
2020-07-30 15:37:58 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,5 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 
-titles = doc.css(".title-oE5vT4")
+p doc.css(".title-oE5vT4")
 
-titles.each do |title| 
-  puts title.text.strip 
-end 
